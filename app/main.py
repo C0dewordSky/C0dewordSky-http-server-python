488961f18616d764219b014ee27b1997e0a116c2
Aakash <aakashsinha1606@gmail.com>
2024-06-01 11:06:38 +0530

    pass stage

--- app/main.py
+++ app/main.py
@@ -12,12 +12,17 @@ def main():
     conn, _ = server_socket.accept() # wait for client
 
     request = conn.recv(1024) 
-    if request.decode("utf-8").split()[1] == "/":
-        conn.sendall(b"HTTP/1.1 200 OK\r\n\r\n") 
-    else:
-        conn.sendall(b"HTTP/1.1 404 Not Found\r\n\r\n")
-    conn.close
+  # if request.decode("utf-8").split()[1] == "/":
+  #     conn.sendall(b"HTTP/1.1 200 OK\r\n\r\n") 
+  #  else:
+  #      conn.sendall(b"HTTP/1.1 404 Not Found\r\n\r\n")
+  #  conn.close"""
     
+    if request.decode("utf-8").split()[1] == "/echo/":
+        conn.sendall(f"HTTP/1.1 200 OK\r\nContent-Type: {str}\r\nContent-Length: {len(str)}\r\n\r\n{str}")
+    else:
+        conn.sendall(b"HTTP/1.1 404 Not Found\r\n\r\n")    
+    conn.close
     
 if __name__ == "__main__":
     main()
