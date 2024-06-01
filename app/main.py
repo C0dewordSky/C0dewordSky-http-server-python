784f21058d20babceff9261a48fe46416e31fd2f
Aakash <aakashsinha1606@gmail.com>
2024-06-01 11:21:40 +0530

    pass stage

--- app/main.py
+++ app/main.py
@@ -9,20 +9,27 @@ def main():
     # Uncomment this to pass the first stage
     #
     server_socket = socket.create_server(("localhost", 4221), reuse_port=True)
-    conn, _ = server_socket.accept() # wait for client
-
-    request = conn.recv(1024) 
-  # if request.decode("utf-8").split()[1] == "/":
-  #     conn.sendall(b"HTTP/1.1 200 OK\r\n\r\n") 
-  #  else:
-  #      conn.sendall(b"HTTP/1.1 404 Not Found\r\n\r\n")
-  #  conn.close"""
     
-    if request.decode("utf-8").split()[1] == "/echo/":
-        conn.sendall(b"HTTP/1.1 200 OK\r\nContent-Type: {str}\r\nContent-Length: {len(str)}\r\n\r\n{str}")
-    else:
-        conn.sendall(b"HTTP/1.1 404 Not Found\r\n\r\n")    
-    conn.close()
+conn, _ = server_socket.accept() # wait for client
+
+request = conn.recv(1024)
+    
+decoded_request = request.decode("utf-8")
+path = decoded_request.split()[1]
+
+if path.startswith("/echo/"):
+    response_body = path[len("/echo/"):]
+    response = (
+        f"HTTP/1.1 200 OK\r\n"
+        f"Content-Type: {response_body}\r\n"
+        f"Content-Length: {len(response_body)}\r\n\r\n"
+        f"{response_body}"
+    ).encode("utf-8")
+    conn.sendall(response)
+else:
+    conn.sendall(b"HTTP/1.1 404 Not Found\r\n\r\n")
+
+conn.close()
     
 if __name__ == "__main__":
     main()
