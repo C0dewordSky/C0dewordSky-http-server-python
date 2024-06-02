18ad03584da60121a14c58e93bf6ca084de28d54
Aakash <aakashsinha1606@gmail.com>
2024-06-02 13:09:05 +0530

    pass stage

--- app/main.py
+++ app/main.py
@@ -1,3 +1,4 @@
+"""
 import socket
 import sys
 import os
@@ -64,6 +65,45 @@ def get_header(request_str):
         if line.startswith("User-Agent:"):
             return line.split(":", 1)[1].strip()
     return None
+
+if __name__ == "__main__":
+    main()
+"""
+
+import socket
+import threading
+import sys
+
+def main():
+    def handle_req(client, addr):
+        data = client.recv(1024).decode()
+        req = data.split('\r\n')
+        path = req[0].split(" ")[1]
+        if path == "/":
+            response = "HTTP/1.1 200 OK\r\n\r\n".encode()  
+        elif path.startswith('/echo'):
+            response = f"HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: {len(path[6:])}\r\n\r\n{path[6:]}".encode()
+        elif path.startswith("/user-agent"):
+            user_agent = req[2].split(": ")[1]
+            response = f"HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: {len(user_agent)}\r\n\r\n{user_agent}".encode()
+        elif path.startswith("/files"):
+            directory = sys.argv[2]
+            filename = path[7:]
+            print(directory, filename)
+            try:
+                with open(f"/{directory}/{filename}", "r") as f:
+                    body = f.read()
+                response = f"HTTP/1.1 200 OK\r\nContent-Type: application/octet-stream\r\nContent-Length: {len(body)}\r\n\r\n{body}".encode()
+            except Exception as e:
+                response = f"HTTP/1.1 404 Not Found\r\n\r\n".encode()
+        else:
+            response = "HTTP/1.1 404 Not Found\r\n\r\n".encode()
+        client.send(response)
+    server_socket = socket.create_server(("localhost", 4221), reuse_port=True)
+    while True:    
+        client, addr = server_socket.accept()
+        threading.Thread(target=handle_req, args=(client, addr)).start()
+    
 
 if __name__ == "__main__":
     main()
